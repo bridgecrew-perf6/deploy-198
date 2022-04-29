5a65aee625d68d0ba4539b29bdd590aa7f3ef401
Cristian Arando <crisarandosyse@gmail.com>
2022-04-28 23:28:45 -0400

    de

---
+++ assets/js/firebase.js
@@ -0,0 +1,108 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1WJuRzxe5M5wtvRx-BK6c7mOps4usdR4",
+  authDomain: "long-old-live-plus-aab44.firebaseapp.com",
+  projectId: "long-old-live-plus-aab44",
+  storageBucket: "long-old-live-plus-aab44.appspot.com",
+  messagingSenderId: "371071760445",
+  appId: "1:371071760445:web:b44da528fddb82bb759254"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+import { getFirestore, collection, addDoc, doc, setDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js"
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-storage.js"
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+
+
+//conexion a Firestore
+const cloudDB = getFirestore();
+//Conexion a Storage
+const storage = getStorage();
+
+const auth = getAuth();
+//var progress
+export const guardarRegistro = (nombre, descripcion, musculo, minutos, segundos, gif) => {
+  const storageRef = sRef(storage, 'gifs/' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)+".gif");
+  const uploadTask = uploadBytesResumable(storageRef, gif);
+  uploadTask.on('state_changed', (snapshot) => {
+    progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    if (progress < 100) {
+          Swal.fire({
+
+            title: "Guardando registro...",
+            icon: "info",
+            closeOnConfirm: true,
+            closeOnCancel: true,
+            allowOutsideClick: false,
+            showCancelButton: false,
+            showConfirmButton: false,
+    
+    
+          });
+        } 
+      
+    },(error) => {
+      alert("error: gif no subido");
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+        guardarFirestore(downloadURL);
+                 
+          setTimeout( function(){
+           
+          },3000);
+
+          setTimeout( function(){
+            Swal.close();
+            window.location.href="ejerciciosFisicos.html";
+          },3000);
+          
+        
+      });
+    }
+    );
+    function guardarFirestore(downloadURL){
+      addDoc(collection(cloudDB, "Ejercicio"),{
+        NombreEjercicio: nombre,
+        DescripcionEjercicio: descripcion,
+        GrupoMuscular: musculo,
+        MinutosEjercicio: minutos,
+        SegundosEjercicio: segundos,
+        GifURL: downloadURL
+      });
+    }
+  }
+
+export const autenticacion = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log("Usuario logeado")
+    
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode + errorMessage)
+  });
+
+}
+
+//export const conf = initializeApp(firebaseConfig);
+
+//export const db = getFirestore();
+
+export const onGetTasks = (callback) =>
+  onSnapshot(collection(cloudDB, "Ejercicio"), callback);
+
+export const onGetTasks2 = (callback) =>
+  onSnapshot(collection(cloudDB, "Rutinas"), callback);
---
+++ assets/js/registrarEjercicio.js
@@ -0,0 +1,81 @@
+import { guardarRegistro } from './firebase.js'
+
+
+const formulario = document.getElementById('formulario');
+const input = document.querySelector('input[type="file"]');
+var gif = [];
+input.addEventListener('change', function (e) {
+    gif = input.files;
+});
+
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                } else {
+                    event.preventDefault();
+                    registrar();
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+async function registrar() {
+
+    event.preventDefault();
+    const { value: test } = await Swal.fire({
+        title: "Registro",
+        text: "¿Estas seguro de registrar el ejercicio?",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#FD7E14",
+        confirmButtonText: "Si, guardar",
+        cancelButtonText: "No, cancelar",
+        cancelButtonColor: "#C4C4C4",
+        closeOnConfirm: false,
+        closeOnCancel: false,
+        
+
+    });
+
+    if (test) {
+
+        const nombre = formulario['nombre']
+        const descripcion = formulario['descripcion']
+        const musculo = formulario['seleccione']
+        const minutos = formulario['min']
+        const segundos = formulario['seg']
+        
+
+        try {
+    
+            guardarRegistro(nombre.value,descripcion.value,musculo.value,minutos.value,segundos.value,gif[0])
+          
+        } catch (error) {
+          console.log(error);
+        }
+
+        
+    } else {
+
+        Swal.fire({
+            title: "¡Cancelado!",
+            icon: "error",
+            timer: 3000
+        });
+
+    }
+
+
+}
